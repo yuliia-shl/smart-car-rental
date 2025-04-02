1b2c5f1190f5143329507b8ab5b82f04e1975259
Yuli <yu.expert@gmail.com>
2025-04-02 23:06:35 +0300

    feat: add and style NotFoundPage

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import s from './NotFound.module.css';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={s.wrapper}>
+      <Link to="/">
+        <Button label="Home Page" size="small" color="secondary" />
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
